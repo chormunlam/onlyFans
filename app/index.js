b1aba15b466b035dd313ac2daec42a024c25cb73
Chormun <chormunlam@yahoo.com.hk>
2023-06-26 17:21:56 -0400

    start

---
+++ app/index.js
@@ -0,0 +1,20 @@
+import { StyleSheet, Text, View } from "react-native";
+
+export default function Page() {
+  return (
+    <View style={styles.container}>
+      <View style={styles.userCard}>
+        {/*Image */}
+        {/*Image */}
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    padding: 24,
+  },
+});
