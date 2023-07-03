35bc817de3282315eea99e6e985d5372122ce69f
Chormun <chormunlam@yahoo.com.hk>
2023-07-03 17:10:17 -0400

    userImage on user card

--- app/index.js
+++ app/index.js
@@ -1,11 +1,28 @@
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, Image } from "react-native";
 
 export default function Page() {
   return (
     <View style={styles.container}>
       <View style={styles.userCard}>
+        {/*I mage */}
+        <Image
+          src="https://notjustdev-dummy.s3.us-east-2.amazonaws.com/avatars/elon.png"
+          style={styles.userImage}
+        />
         {/*Image */}
-        {/*Image */}
+        <View>
+          <Text
+            style={{
+              color: "white",
+              fontSize: 22,
+              fontWeight: "500",
+              marginBottom: 5,
+            }}
+          >
+            Elon Musk
+          </Text>
+          <Text>@eleonmusk</Text>
+        </View>
       </View>
     </View>
   );
@@ -14,7 +31,20 @@ export default function Page() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: "center",
-    padding: 24,
+    padding: 10,
+  },
+  userCard: {
+    backgroundColor: "gray",
+    padding: 10,
+    flexDirection: "row",
+    alignItems: "flex-end",
+  },
+  userImage: {
+    width: 100,
+    height: 100,
+    borderRadius: 100,
+    borderColor: "white",
+    borderWidth: 3,
+    marginRight: 20,
   },
 });
